a33b4bed7a7d39beb5d98a83df4aebb8e52b018c
otavioxavier <otavioxaviermarinho@gmail.com>
2024-07-31 17:29:25 -0300

    :bug: fix: prismadb eslint

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -1,11 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-declare global{
-  let prisma: PrismaClient | undefined;
-}
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-const prismadb = globalThis.prisma || new PrismaClient();
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-if(process.env.NODE_ENV !== 'production') globalThis.prisma = prismadb;
+const prismadb = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prismadb;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prismadb;
