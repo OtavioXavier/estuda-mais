7d19047edeadf9071685eb0d8fe4826a2d36ea82
otavioxavier <otavioxaviermarinho@gmail.com>
2024-08-01 22:53:28 -0300

    :recycle: refactor: add ';'

--- src/dto/update-matter.dto.ts
+++ src/dto/update-matter.dto.ts
@@ -1,6 +1,5 @@
 export type UpdateMatterDto = {
-  name: string | null;
-  studyDays: number[] | null;
-  status: boolean | null;
-  updated_at: Date;
+  name?: string;
+  studyDays?: number[];
+  status?: boolean;
 };
