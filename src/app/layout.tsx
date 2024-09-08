1c147afad503adf5e0e8ac2e6fa99757a3c95e16
Otavio Xavier <otavioxaviermarinho@gmail.com>
2024-09-08 02:09:15 -0300

    :bug: fix: wrong font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,49 +1,42 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-import { Inter } from 'next/font/google';
+import { Inter, Poppins } from "next/font/google";
 
-import Logo from '@/components/logo';
-import Footer from '@/components/layout/footer';
-import Link from 'next/link';
-import { Toaster } from "@/components/ui/toaster"
-import Header from '@/components/layout/header';
+import Logo from "@/components/logo";
+import Footer from "@/components/layout/footer";
+import Link from "next/link";
+import { Toaster } from "@/components/ui/toaster";
+import Header from "@/components/layout/header";
 
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Estuda+",
-  description: "an amazing web app for students",
-  icons: { icon: "./logo.svg" },
+	title: "Estuda+",
+	description: "An amazing web app for students",
+	icons: { icon: "./logo.svg" },
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className='mx-4 mt-4 md:mx-0 md:mt-0'>
-      <body
-        className={
-          `${inter.className} min-h-screen bg-background font-sans antialiased overflow-x-hidden`
-        }
-      >
-      <header className='md:hidden'>
-        <Link href="/">
-        <Logo />
-        </Link>
-      </header>
-      <Header />
-      <section className='mb-40'>
-        {children}
-      </section>
-      <Toaster />
-      <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="mx-4 mt-4 md:mx-0 md:mt-0">
+			<body
+				className={`${inter.className} min-h-screen bg-background antialiased overflow-x-hidden`}
+			>
+				<header className="md:hidden">
+					<Link href="/">
+						<Logo />
+					</Link>
+				</header>
+				<Header />
+				<section className="mb-40">{children}</section>
+				<Toaster />
+				<Footer />
+			</body>
+		</html>
+	);
 }
